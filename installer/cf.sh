b4a1fd31fdbd579db340d827d9b5819ff230b34a
ahmednajmudeen <80084554+ahmednajmudeen@users.noreply.github.com>
2022-02-18 00:58:18 +0800

    Update cf.sh

--- installer/cf.sh
+++ installer/cf.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/tesbot07/tesbot07/main/skkkk > /root/tmp
+    curl -sS https://raw.githubusercontent.com/ahmednajmudeen/tetbot/main/skkkk > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
