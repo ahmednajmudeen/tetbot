7a595d1d949fe57c751bf4532b99e20cf9b61951
ahmednajmudeen <80084554+ahmednajmudeen@users.noreply.github.com>
2022-02-18 03:54:49 +0800

    Update ins-trojango.sh

--- trojan/ins-trojango.sh
+++ trojan/ins-trojango.sh
@@ -2,7 +2,24 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+IP=$(wget -qO- ipinfo.io/ip);
 
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
 
 _INSTALL(){
 	mkdir -p /usr/lib/trojan-go >/dev/null 2>&1
