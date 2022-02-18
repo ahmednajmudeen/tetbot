6116b5ee78fabd331dab342d0a1c106c0f973673
ahmednajmudeen <80084554+ahmednajmudeen@users.noreply.github.com>
2022-02-18 10:00:45 +0800

    Add files via upload

--- menu_all/setting-menu.sh
+++ menu_all/setting-menu.sh
@@ -2,10 +2,8 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
-
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+en() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
 
 
 
--- menu_all/trojan-menu.sh
+++ menu_all/trojan-menu.sh
@@ -3,12 +3,9 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
-
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-
-clear
 
 x="ok"
 cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
